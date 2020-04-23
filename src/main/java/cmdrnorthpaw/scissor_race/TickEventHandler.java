6f7331af78d4504240f9bf1ce001284b0026729f
CmdrNorthpaw <thetuxkid@gmail.com>
2020-04-23 13:05:47 +0100

    Player is now damaged when sprinting with shears.

---
+++ src/main/java/cmdrnorthpaw/scissor_race/TickEventHandler.java
@@ -0,0 +1,27 @@
+package cmdrnorthpaw.scissor_race;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.util.DamageSource;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class TickEventHandler {
+    @SubscribeEvent
+    public static void serverTickEvent(TickEvent.PlayerTickEvent event) {
+        PlayerEntity player = event.player;
+        if (player.isSprinting()) {
+            Item heldItem = player.getHeldItemMainhand().getItem();
+            Item shears = Item.getItemById(359);
+            if (heldItem == shears) {
+                player.attackEntityFrom(DamageSource.CACTUS, 1.0F);
+
+            }
+        }
+    }
+}
+
